db48f87d66d0a1a015d619b47a444f2dd9432835
oren <oren@gmai.com>
2024-06-26 15:48:29 +0300

    feat: clean garbage - dont let model play more then his turn in the conversation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::Read;
 use clap::Parser;
 use kalosm_llama::prelude::*;
 use tokio;
+use regex::Regex;
 
 const _END_OF_TURN_TOKEN: &str = &"ROBOT:";
 const _TEST_PROMPT: &str = &"A vulnerability was found in Schneider Electric APC Easy UPS Online up to 2 --- A vulnerability, which was classified as critical, was found in Apple Safari up to 15 --- A vulnerability classified as critical was found in D-Link DIR-895 FW102b07 (Router Operating System) --- A vulnerability, which was classified as critical, was found in Microsoft Edge 99 --- A vulnerability classified as problematic was found in Huawei HarmonyOS and EMUI (affected version not known)";
@@ -38,20 +39,38 @@ fn read_prompt_from_file(path: &str) -> String {
     contents
 }
 
+
+fn clean_garbage_text(text: &str, garbage_patterns: Vec<&str>) -> String {
+    let mut cleaned_text = String::from(text);
+    for pattern in garbage_patterns {
+        let re = Regex::new(pattern).expect(&format!("Failed to create regex pattern {}", pattern));
+        cleaned_text = re.replace_all(&cleaned_text, "").to_string();
+    }
+    cleaned_text
+}
+
+
+const ROBOT_TURN_HEADER: &'static str = "ROBOT:\n";
+
+const USER_TURN_HEADER: &'static str = "USER:\n";
+
 async fn chat_main(model: Llama, seed: String) {
     let mut last_generated = String::from("ROBOT:\n...\n");
     loop {
         println!("\n\nEnter a prompt: ");
-        let user_prompt = format!("{}\n{}\nUSER:\n{}\nROBOT:\n", seed, last_generated, read_user_input());
+        let user_prompt = format!("{}\n{}\n{}{}\nROBOT:\n", seed, last_generated, USER_TURN_HEADER, read_user_input());
         // println!("\n\n<DEBUG(prompt)>\n{}</DEBUG>\n\n", user_prompt);
         let mut result = model.stream_text(&user_prompt).await.expect("Failed to stream text");
         println!();
-        last_generated = String::from("ROBOT:\n");
+        last_generated = String::from(ROBOT_TURN_HEADER);
         while let Some(token) = result.next().await {
-            print!("{token}");
+            // print!("{token}");
             last_generated.push_str(&format!("{token}"));
         }
+        let user_impersonation_pattern = "(?s)USER:\n.+";
+        last_generated = clean_garbage_text(&last_generated, vec![user_impersonation_pattern]);
         last_generated.push_str("\n");
+        println!("{}", last_generated);
     }
 }
 
